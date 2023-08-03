a0c44cb9a8d888d646f789e8aa1112481faa54c4
devillived8 <il.volobuev2014@yandex.ru>
2023-08-04 00:33:30 +0300

    добавил favicon и базовую разметку на авторизацию

---
+++ authorization.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/reset.css">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>dsoSite</title>
+</head>
+
+<body>
+    <!-- HEADER -->
+    <header class="header">
+        <div class="wrapper">
+            <a href="#">
+                <img src="./img/logo.png" alt="logo" class="logo">
+            </a>
+        </div>
+    </header>
+    <!-- HEADER -->
+    <!-- MAIN -->
+    <main class="main">
+        <div class="wrapper">
+            
+        </div>
+    </main>
+    <!-- MAIN -->
+    <!-- FOOTER -->
+    <footer class="footer">
+        <div class="wrapper">
+            <a href="https://vk.com/devillived8" target="_blank" rel="author" class="author">
+                <p>Создатель</p>
+                <img class="vkIcon" src="./img/vkIcon.png" width="60px" alt="vkIcon">
+            </a>
+            <a href="https://vk.com/id229840958" target="_blank" rel="author" class="author">
+                <p>Создатель</p>
+                <img class="vkIcon" src="./img/vkIcon.png" width="60px" alt="vkIcon">
+            </a>
+        </div>
+    </footer>
+    <!-- FOOTER -->
+    <!-- SCRIPTS -->
+    <script src="./js/main.js"></script>
+    <!-- SCRIPTS -->
+</body>
+</html>
